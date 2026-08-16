5b645e14f6520f135198c8c1ef55d7d4de3314c0
VadymHromyk <hromuk.vadim@gmail.com>
2026-08-16 22:22:59 +0300

    add app.listen fn

--- src/server.js
+++ src/server.js
@@ -45,3 +45,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
